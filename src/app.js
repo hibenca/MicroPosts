baaa2bb47089b6ca5bfcef471f51aa2610c03878
Bluesmoke <43018850+hibenca@users.noreply.github.com>
2020-09-12 09:48:16 -0500

    Add function to delete button

--- src/app.js
+++ src/app.js
@@ -4,13 +4,17 @@ import { ui } from './ui'
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
-// Submit post
+// Listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
+// Get Posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
-  .then(data => ui.showPosts(data))
-  .catch(err => console.log(err));
+    .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
 }
 
 // Submit post
@@ -23,13 +27,31 @@ function submitPost() {
     body
   }
 
-
   // Create post
   http.post('http://localhost:3000/posts', data)
-  .then(data => {
-    ui.showAlert('Post added', 'alert alert-success');
-    ui.clearFields();
-    getPosts()
-  })
-  .catch(err => console.log(err))
+    .then(data => {
+      ui.showAlert('Post added', 'alert alert-success');
+      ui.clearFields();
+      getPosts()
+    })
+    .catch(err => console.log(err))
 }
+
+
+// Delete post
+function deletePost(e) {
+  e.preventDefault();
+  if (e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+    if (confirm('Are you sure?')) {
+      http
+        .delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.showAlert('Post removed', 'alert alert-success');
+          getPosts();
+        })
+        .catch(err => console.log(err))
+    }
+  }
+}
+
